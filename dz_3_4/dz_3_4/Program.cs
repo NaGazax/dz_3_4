913bbc3ede69ced4b79d6168c63114d5dccd414f
Artem <temka6397@mail.ru>
2022-07-08 21:50:57 +0500

    zadanie1

---
+++ dz_3_4/dz_3_4/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите пятизначное число:");
+string firstNumber = Console.ReadLine();  
+int secondNumber = Convert.ToInt32(firstNumber);
+if (secondNumber < 10000 | secondNumber >100000) 
+{
+    Console.WriteLine("Неверное число");
+} 
+else
+{
+    if (firstNumber[0] == firstNumber[4] && firstNumber[1] == firstNumber[3])
+    {
+        Console.WriteLine("Полиндром");
+    }
+    
+    else
+    {
+         Console.WriteLine("Не полиндром");
+    }
+}  
+    
