fe893647e9c35a73ca41da21112b6fdf844d0bc7
Artem <temka6397@mail.ru>
2022-07-14 22:44:21 +0500

    Zadanie7 ver2

--- dz_3_4/dz_3_4/Program.cs
+++ dz_3_4/dz_3_4/Program.cs
@@ -5,7 +5,7 @@ Console.WriteLine("Случайный массив");
 for (int i = 0; i < array.Length; i++)
 {
    array[i] = new Random().Next(100, 999);
-    Console.Write("{0} ", array[i]);
+   Console.WriteLine($"[{String.Join(";",array)}]");
 }
 foreach(int element in array)
 {
