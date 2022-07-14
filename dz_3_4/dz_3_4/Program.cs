536abb6798c59d6b44f0a7337aba80e487951076
Artem <temka6397@mail.ru>
2022-07-14 22:41:36 +0500

    Zadanie8

--- dz_3_4/dz_3_4/Program.cs
+++ dz_3_4/dz_3_4/Program.cs
@@ -1,20 +1,23 @@
 ﻿
 int[] array = new int[6];
 int around = 0;
+int sum = 0;
 Console.WriteLine("Случайный массив");
 for (int i = 0; i < array.Length; i++)
 {
    array[i] = new Random().Next(100, 999);
-    Console.Write("{0} ", array[i]);
+  Console.WriteLine($"[{String.Join(";",array)}]");
 }
+
 foreach(int element in array)
 {
-   Console.Write("{0} ", element);
-     if (element % 2 == 0)
+     if (element % 2 != 0)
       {
         around++;
-        
+        sum += element;
      }
   }
 Console.WriteLine();
-Console.WriteLine("Количество четных элементов {0}" , around);
+Console.WriteLine("Количество  нечетных элементов: {0}" , around);
+Console.WriteLine();
+Console.WriteLine("Сумма  нечетных элементов: {0}" , sum);
