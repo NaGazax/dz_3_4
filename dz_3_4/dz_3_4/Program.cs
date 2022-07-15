0b4317e9537a332e9949a25f77ad1ea84d198ae6
Artem <temka6397@mail.ru>
2022-07-15 22:59:31 +0500

    zadanie9

--- dz_3_4/dz_3_4/Program.cs
+++ dz_3_4/dz_3_4/Program.cs
@@ -1,20 +1,14 @@
 ﻿
 int[] array = new int[6];
-int around = 0;
-Console.WriteLine("Случайный массив");
-for (int i = 0; i < array.Length; i++)
-{
-   array[i] = new Random().Next(100, 999);
-   Console.WriteLine($"[{String.Join(";",array)}]");
+int max = 0;
+int min = 0;
+for (int i = 0; i < array.Length; i++)  
+{ 
+array[i] = new Random().Next(100, 999);
+max = array.Max();
+min = array.Min();
 }
-foreach(int element in array)
-{
-   Console.Write("{0} ", element);
-     if (element % 2 == 0)
-      {
-        around++;
-        
-     }
-  }
-Console.WriteLine();
-Console.WriteLine("Количество четных элементов {0}" , around);
+Console.WriteLine($"({String.Join(";",array)})");
+Console.WriteLine($"Максимальное число :  {max} ");
+Console.WriteLine($"Минимальное число :  {min} ");
+Console.WriteLine($"Разница двух чисел :  {max - min} ");
