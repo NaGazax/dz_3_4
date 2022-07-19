3c40e14259f61f81943ec5a7a1ec9e8a0305b75a
Artem <temka6397@mail.ru>
2022-07-19 22:25:51 +0500

    zadanie9

--- dz7/Program.cs
+++ dz7/Program.cs
@@ -11,21 +11,39 @@
 // }
 //  Console.WriteLine($"Количество положительных чисел: {count}"); 
 
-Console.Write("Введите k1: ");
-var k1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите b1: ");
-var b1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите k2: ");
-var k2 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите b2: ");
-var b2 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите k1: ");
+// var k1 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите b1: ");
+// var b1 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите k2: ");
+// var k2 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите b2: ");
+// var b2 = Convert.ToDouble(Console.ReadLine());
  
  
-var x = (b2 - b1) / (k1 - k2);
-var y = k1 * x + b1;
+// var x = (b2 - b1) / (k1 - k2);
+// var y = k1 * x + b1;
 
-if ( k1 == k2) Console.WriteLine( "Прямые параллельны");
-x = Math.Round(x);
-y = Math.Round(y);
+// if ( k1 == k2) Console.WriteLine( "Прямые параллельны");
+// x = Math.Round(x);
+// y = Math.Round(y);
  
-Console.WriteLine($"Пересечение в точке: ({x};{y})");
+// Console.WriteLine($"Пересечение в точке: ({x};{y})");
+
+
+Console.Write("Введите количество строк ");
+int rows = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов ");
+int columns = Convert.ToInt32(Console.ReadLine());
+double [,] matrix = new double[rows,columns];
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i,j] = new Random().NextDouble()* 100;
+        Console.Write(matrix[i,j] + "\t"); // Литералы
+}
+Console.WriteLine();
+}
+
